f1bf0089f18e87da81c4f5a0b1eb282f052e9419
Szymon <s.walach@soniqsoft.pl>
2021-11-30 11:32:44 +0100

    initialize new ract app with typescript

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/api/posts", postsRouter)
 app.use("/api/comments", commentsRouter)
 app.use("/api/users", usersRouter)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 connectDB(process.env.MONGO_URI)
 app.listen(PORT, () => console.log(`Server listen on http://localhost:${PORT}`))
